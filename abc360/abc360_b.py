240a13a6baadbf51d7169d0e1907368718aa4453
yummyweb <antriksh.timepass@gmail.com>
2024-07-04 23:29:46 +0530

    solved abc360_b

--- abc360/abc360_b.py
+++ abc360/abc360_b.py
@@ -5,13 +5,29 @@ t = inp.split(" ")[1]
 # atcoder:
 # a, t, c, o, d, e, r
 
+found = False
+
 # w goes up till len(s) - 1
 for w in range(1, len(s)):
     # this loop checks for splitting and concatenation
-    line = ""
+    line = []
+    new = ""
     for i in range(1, len(s)+1):
-        line += s[i-1]
-        if i % w == 0:
-            line += "\n"
+        new += s[i-1]
+        if i % w == 0 or i == len(s):
+            line.append(new)
+            new = ""
 
-    print(line)
+    for i in range(0, w):
+        res = ""
+        for l in line:
+            if len(l) >= i+1:
+                res += l[i]
+
+        if res == t:
+            found = True
+
+if found:
+    print("Yes")
+else:
+    print("No")
